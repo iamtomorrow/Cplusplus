b8bc79c5093f4f7246ac278225f43ec352e15575
Talisson <72582696+iamtomorrow@users.noreply.github.com>
2020-11-12 19:45:58 -0300

    Update and rename 002out.cpp to 0002out.cpp

---
+++ 0002out.cpp
@@ -0,0 +1,14 @@
+//C++ Syntax
+
+#include <iostream>           // header file library <iostream>
+
+using namespace std;          // we can use names for objects and variables from the standard library
+
+int main ()                   // funtion main
+{                             // { curly brackets
+
+   cout << "Hello world";     // an object "see-out" and semicolon (;)
+
+   return 0;                  // ends the main function
+
+}                             //} curly brackets
