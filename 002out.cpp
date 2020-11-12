502f974468b980ba4c0d6bc9ed52d70b06db6cf6
Talisson <72582696+iamtomorrow@users.noreply.github.com>
2020-11-12 19:42:40 -0300

    Add files via upload

---
+++ 002out.cpp
@@ -0,0 +1,13 @@
+//C++ Syntax
+#include <iostream>           //header file library
+
+using namespace std;          //we can use names for objects and variables from the standard library
+
+int main ()                   //Funtion
+{                             //{ curly brackets
+
+   cout << "Hello world";     //An object "see-out" and semicolon (;)
+
+   return 0;                  //Ends the main function
+
+}                             //} curly brackets
