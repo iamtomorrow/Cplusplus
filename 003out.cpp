acebb1e8165356ca54f9d60f7da572332a15520d
Talisson <72582696+iamtomorrow@users.noreply.github.com>
2020-11-12 19:47:19 -0300

    0003out.cpp

---
+++ 003out.cpp
@@ -0,0 +1,13 @@
+//C++ Output (Print Text)
+#include <iostream>
+#include <string.h>
+
+int main ()
+{
+
+    std::cout << strlen("Hello world!\n\n");
+    //std::cout << "I am learning C++!";
+
+    return 0;
+
+}
