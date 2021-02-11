91f311fdafb5064cae031051f8f700dbf646fbda
Tomorrow, Corp <72582696+iamtomorrow@users.noreply.github.com>
2021-02-11 01:12:56 -0300

    Add files via upload

---
+++ functions/out001.cpp
@@ -0,0 +1,24 @@
+// Creating a Function in cpp
+
+#include <iostream>
+
+using namespace std;
+
+// My 1st function;
+void my1stFunction() {
+  cout << "Hello cpp" << endl;
+}
+
+// My 2nd function;
+void my2ndFunctionn(string name) {
+  cout << "Hello " << name << " I am yor computer";
+}
+
+int main(void) {
+
+  my1stFunction();
+  my2ndFunctionn("Talisson");
+
+  return 0;
+
+}
